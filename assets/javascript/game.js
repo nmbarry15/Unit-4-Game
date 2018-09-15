a7662ec066e57f4b6778b2414382cfe20b08fea2
Nicole Barry <nmbarry15@gmail.com>
2018-09-14 20:55:46 -0400

    simplify button click in jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,15 +11,20 @@ var gameActive = false;
 
 
 var reset = function () {
+    //set picture values to random numbers 1-10
+    //I am having trouble making it so each picture has a different value
     for (var i = 0; i < picIds.length; i++) {
         nums[i] = Math.floor(Math.random() * 10) + 1;
         $(picIds[i]).attr("value", nums[i]);
         console.log(nums[i]);
     }
     console.log(nums);
+    //get random number for use to get to 30-100
     randomNum = Math.floor(Math.random() * 70) + 30;
+    //reset user number
     userNum = 0;
     gameActive = true;
+    //print numbers to screen and hide win/lose statement and play again button
     $("#random-number").html(randomNum);
     $("#user-number").html(userNum);
     $("#wins").html(wins);
@@ -30,10 +35,12 @@ var reset = function () {
 
 var btnClick = function () {
     if (gameActive) {
+        //get value of pic and add to user number
         var num = parseInt($(this).attr("value"));
         userNum += num;
         console.log("user " + userNum);
         console.log("random " + randomNum);
+        //wins
         $("#user-number").html(userNum);
         if (userNum === randomNum) {
             console.log("you win!")
@@ -43,6 +50,7 @@ var btnClick = function () {
             $("#win-lose").html("<h3>Awesome! You are King of the Jungle!</h3>");
             $("#play-again").show();
         }
+        //losses
         if (userNum > randomNum) {
             console.log("you lose!")
             losses++;
@@ -56,10 +64,10 @@ var btnClick = function () {
 
 reset();
 
-$("#elephant").click(btnClick);
-$("#hippo").click(btnClick);
-$("#monkey").click(btnClick);
-$("#lion").click(btnClick);
+//run btnClick fuction when each button is clicked
+$(".button").click(btnClick);
+
+//run reset function when play again button is called
 $("#play-again").click(reset);
 
 
